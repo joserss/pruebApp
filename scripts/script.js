0d0b4fdcab0b846b0b44cbf74cf28721bd3a0895
joserss <josee.88@gmail.com>
2021-01-15 18:25:47 -0700

    .

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,21 @@
  * limitations under the License.
  * =============================================================================
  */
+navigator.mediaDevices.enumerateDevices().then(function (devices) {
+    for(var i = 0; i < devices.length; i ++){
+        var device = devices[i];
+        if (device.kind === 'videoinput') {
+            var option = document.createElement('option');
+            option.value = device.deviceId;
+            option.text = device.label || 'camera ' + (i + 1);
+            document.querySelector('select#videoSource').appendChild(option);
+        }
+    };
+});
+
+
+
+
 
 const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
